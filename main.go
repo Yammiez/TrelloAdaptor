a9d5827679e76fbd2efe456a382ee2166f2a1ca4
Yammiez <Edward.KarYun.Yam@gmail.com>
2016-10-15 09:22:09 -0700

    Test adding a card

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "log"
 
     "github.com/VojtechVitek/go-trello"
+
 )
 
 func main() {
@@ -18,13 +19,13 @@ func main() {
 	dat, err = ioutil.ReadFile("W:\\GoWork\\key\\Trello.com\\builder\\token")
 	token := string(dat)
 
-	trello, err := trello.NewAuthClient(appKey, &token)
+	trelloClient, err := trello.NewAuthClient(appKey, &token)
     if err != nil {
         log.Fatal(err)
     }
 
     // User @trello
-    user, err := trello.Member("edwardyam")
+    user, err := trelloClient.Member("builder25")
     if err != nil {
         log.Fatal(err)
     }
@@ -35,25 +36,44 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
+
+/*	
+	if len(boards) > 0 {
+		board := boards[0]
+	} else {
+		panic("No boards found!")
+	}
+*/
+	board := boards[0]
 	
-    if len(boards) > 0 {
-        board := boards[0]
-        fmt.Printf("* %v (%v)\n", board.Name, board.ShortUrl)
+	// Get TestBoard
+	for _, boardIterator := range(boards) {
+	    if boardIterator.Name == "TestBoard" {
+			fmt.Printf("Found TestBoard!\n")
+			board = boardIterator
+		}
+	}
+	
+	fmt.Printf("* %v (%v)\n", board.Name, board.ShortUrl)
+	
+	// @trello Board Lists
+	lists, err := board.Lists()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        // @trello Board Lists
-        lists, err := board.Lists()
-        if err != nil {
-            log.Fatal(err)
-        }
+	for _, list := range lists {
+		fmt.Println("   - ", list.Name)
 
-        for _, list := range lists {
-            fmt.Println("   - ", list.Name)
-
-            // @trello Board List Cards
-            cards, _ := list.Cards()
-            for _, card := range cards {
-                fmt.Println("      + ", card.Name)
-            }
-        }
-    }
+		// @trello Board List Cards
+		cards, _ := list.Cards()
+		for _, card := range cards {
+			fmt.Println("      + ", card.Name)
+		}
+	}
+	
+	// Test adding a card to the first list
+	firstList := lists[0]
+	firstList.AddCard(trello.Card{Name : "My New Card", Pos : 1})
+	fmt.Printf("Card added to the first list!\n")
 }
